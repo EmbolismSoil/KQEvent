8dedbec8685b814d8e873e2566af58a4b5363df3
EmbolismSoil <743703241@qq.com>
2016-09-17 03:42:14 +0800

    添加抽象acceptor

--- includes/net/AbstractAcceptor.h
+++ includes/net/AbstractAcceptor.h
@@ -5,10 +5,28 @@
 #ifndef KQEVENT_ABSTRACTACCEPTOR_H
 #define KQEVENT_ABSTRACTACCEPTOR_H
 
-namespace KQEvent{
+#include "Socket.h"
+#include "Connection.h"
+
+namespace KQEvent {
+    /**
+     *  @brief AbstractAcceptor
+     *  Acceptor的职责是监听socket, 当连接事件到来时
+     *  需要生成并且返回一个新的链接
+    */
     class AbstractAcceptor {
     public:
+        virtual Connection::ConnectionPtr acceptHandle() = 0;
+        virtual ~AbstractAcceptor(){};
+        //禁止拷贝
+        AbstractAcceptor(AbstractAcceptor const&) = delete;
+        AbstractAcceptor const&operator=(AbstractAcceptor const&) = delete;
 
+    protected:
+        AbstractAcceptor(Socket::SocketPtr socket);
+
+    private:
+        Socket::SocketPtr _socket;
     };
 }
 
