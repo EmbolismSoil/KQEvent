7651c391e0c8ec0fdbf449eb82f413dd7617d200
EmbolismSoil <743703241@qq.com>
2016-09-18 23:18:02 +0800

    开始TCPServer

---
+++ includes/net/TCPServer.h
@@ -0,0 +1,16 @@
+//
+// Created by lee on 16-9-18.
+//
+
+#ifndef KQEVENT_TCPSERVER_H
+#define KQEVENT_TCPSERVER_H
+
+namespace KQEvent{
+    class TCPServer {
+
+    };
+}
+
+
+
+#endif //KQEVENT_TCPSERVER_H
---
+++ src/net/TCPServer.cpp
@@ -0,0 +1,9 @@
+//
+// Created by lee on 16-9-18.
+//
+
+#include "TCPServer.h"
+
+namespace  KQEvent{
+
+}
